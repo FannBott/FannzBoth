97e6d652a99ad3965d71f0c1ca403f798c172354
FannBott <108573537+FannBott@users.noreply.github.com>
2022-09-23 17:34:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,13 +2,13 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['66827294199'],
+  ['66827294199'],
+  ['66827294199', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = [66827294199] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -56,9 +56,9 @@ fs.watchFile(file_exif, () => {
 })
 
 // Sticker WM
-global.packname = sticker_name
-global.author = sticker_author
-global.wm = '© Aine'
+global.packname = SATRIA - MD
+global.author = PUNYA - SATRIA NI
+global.wm = '© SATRIA - ZYAA
 
 global.multiplier = 1000 // The higher, The harder levelup
 
